d5c4046dca1e93672c4b62bc80d72d0c6bf59930
JoSangYeon <whtkddus98@kookmin.ac.kr>
2022-04-01 13:20:09 +0900

    감정상태 세부 보여주기 기능

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const mysql = require('mysql');
+// const dbsetting = require('./dbsetting');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -7,18 +8,30 @@ const session = require('express-session');
 const crypto = require('crypto');
 const FileStore = require('session-file-store')(session); 
 const cookieParser = require('cookie-parser');
-const sendMailRouter = require('./routes/sendmail');
 
 // express 설정 1
 const app = express();
+
+// db 생성
+// dbsetting.dbinit();
 
 // db 연결 2
 const client = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'cnj140535',
-    database: 'aid'
+    password: '98sy1219pp!',
+    database: 'aid_db'
+});
+
+client.connect((err) => {
+    if (err)
+    {
+        console.log(err)
+        con.end();
+        throw err;
+    }
+    else {console.log("DB 연결 성공");}
 });
 
 // 정적 파일 설정 (미들웨어) 3
@@ -38,7 +51,6 @@ app.use(session({
     saveUninitialized: true, // 세션이 필요하면 세션을 실행시칸다(서버에 부담을 줄이기 위해)
     store: new FileStore() // 세션이 데이터를 저장하는 곳
 }));
-app.use('/', sendMailRouter);
 
 
 
@@ -68,22 +80,34 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
     console.log('회원가입 하는중')
     const body = req.body;
+    const serial = body.serial;
     const id = body.id;
     const pw = body.pw;
     const name = body.name;
     const age = body.age;
+    const address = body.address;
+
+    const rela1_name = body.rela1_name;
+    const rela1 = body.rela1;
+    const rela1_num = body.rela1_number;
+    const rela2_name = body.rela2_name;
+    const rela2 = body.rela2;
+    const rela2_num = body.rela2_number;
+    const rela3_name = body.rela3_name;
+    const rela3 = body.rela3;
+    const rela3_num = body.rela3_number;
 
     client.query('select * from user where USER_ID=?', [id], (err, data) => {
         console.log(data);
         if (data.length == 0) {
             console.log('회원가입 성공');
-            client.query('insert into user(USER_ID, NAME, AGE, PASSWORD) values(?,?,?,?)', [
-                id, name, age, pw
+            client.query('insert into user(Serial_Number, USER_ID, Password, Name, Age, Address, Relationship_1_Name, Relationship_1, Relationship_1_Number, Relationship_2_Name, Relationship_2, Relationship_2_Number, Relationship_3_Name, Relationship_3, Relationship_3_Number) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [
+                serial, id, pw, name, age, address, rela1_name, rela1, rela1_num, rela2_name, rela2, rela2_num, rela3_name, rela3, rela3_num
             ]);
             res.redirect('/');
         } else {
             console.log('회원가입 실패');
-            res.send('<script>alert("회원가입 실패");</script>')
+            res.send('<script>alert("회원가입 실패\n중복된 ID입니다.");</script>');
             res.redirect('/login');
         }
     });
@@ -101,32 +125,38 @@ app.post('/login', (req, res) => {
     const id = body.id;
     const pw = body.pw;
 
-    client.query('select * from user where USER_ID=?', [id], (err, data) => {
+    client.query('select USER_ID, Password, Name, Age, Address from user where USER_ID=?', [id], (err, data) => {
         // 로그인 확인
-        console.log(data[0]);
-        console.log(id);
-        console.log(data[0].USER_ID);
-        console.log(data[0].PASSWORD);
-        console.log(id == data[0].USER_ID);
-        console.log(pw == data[0].PASSWORD);
-        if (id == data[0].USER_ID || pw == data[0].PASSWORD) {
+        // console.log(data[0]);
+        // console.log(id);
+        // console.log(data[0].USER_ID);
+        // console.log(data[0].Password);
+        // console.log(id == data[0].USER_ID);
+        // console.log(pw == data[0].Password);
+        if (data.length == 0)
+        {
+            console.log('등록된 정보가 없습니다.');
+            res.render('login', {});
+        }
+        else if (id == data[0].USER_ID && pw == data[0].Password) {
             console.log('로그인 성공');
             // 세션에 추가
             req.session.is_logined = true;
-            req.session.name = data.NAME;
-            req.session.id = data.USER_ID;
-            req.session.pw = data.PASSWORD;
+            req.session.name = data[0].Name;
+            req.session.user_id = data[0].USER_ID;
+            req.session.user_pw = data[0].Password;
             req.session.save(function () { // 세션 스토어에 적용하는 작업
                 res.render('index', { // 정보전달
-                    name: data[0].NAME,
                     id: data[0].USER_ID,
-                    age: data[0].AGE,
+                    name: data[0].Name,
+                    age: data[0].Aage,
+                    address : data[0].Address,
                     is_logined: true
                 });
             });
         } else {
-            console.log('로그인 실패');
-            res.render('login');
+            console.log('로그인 실패\nID 또는 PW를 확인해주세요');
+            res.render('login', {});
         }
     });
 
@@ -140,6 +170,44 @@ app.get('/logout', (req, res) => {
         // 세션 파괴후 할 것들
         res.redirect('/');
     });
+
+});
+
+// 통계자료 보기
+app.get('/info', (req, res) => {
+    console.log('통계자료 확인하기');
+    console.log(req.session);
+
+    client.query('select * from log where USER_USER_ID=?', [req.session.user_id], (err, data) => {
+        let user_data = {"Happiness":0, "Sadness":0, "Anger":0, "Anxiety":0};
+        let last_time_stemp;
+        let last_sentence;
+
+        for (var i=0; i<data.length; i++)
+        {   
+            user_data[data[i]['Emotion']] += 1;
+        }
+        // 로그인된 사람의 최근 2주간 데이터를 추출해서 
+        // 각 감정 상태에 대한 통계(일단 2주간 각 감정 횟수 정도?)
+        // 를 출력하도록 해야할 듯!
+
+        last_time_stemp = data[i-1]['Time_stemp'];
+        last_sentence = data[i-1]['Text_path'];
+
+        console.log(user_data);
+        console.log(last_sentence);
+        console.log(last_time_stemp);
+
+        res.render('info', {
+            name: req.session.name,
+            info_data: user_data,
+            user_time: last_time_stemp,
+            user_sentence: last_sentence
+        });
+    });
+});
+
+app.post('/info', (req, res) => {
 
 });
 
