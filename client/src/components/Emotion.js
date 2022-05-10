5cb107d496e908aec6f27e81fd061437b9a41517
HideOnHouse <fffuro@kookmin.ac.kr>
2022-05-10 20:05:20 +0900

    Implement Signup, Signin, Login, Dashboard

---
+++ client/src/components/Emotion.js
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Title from './Title';
+import {PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Legend} from "recharts";
+
+function preventDefault(event) {
+    event.preventDefault();
+}
+
+export default function Emotion() {
+
+    const data = [
+        {
+            "name": "우울",
+            "value": 31,
+        },
+        {
+            "name": "우울하지 않음",
+            "value": 79,
+        }
+    ]
+
+    const colors = [
+        "#e57373", "#42a5f5"
+    ]
+
+    const RADIAN = Math.PI / 180;
+
+
+    const renderCustomizedLabel = ({
+                                       cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+                                   }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    }
+
+
+    return (
+        <React.Fragment>
+            <Title>최근 2주간 감정 상태</Title>
+            <ResponsiveContainer>
+                <PieChart width={730} height={250}>
+                    <Pie
+                        data={data}
+                        dataKey="value"
+                        nameKey="name"
+                        cx="50%"
+                        cy="50%"
+                        outerRadius={50}
+                        label={renderCustomizedLabel}
+                        labelLine={false}
+                    >
+                        {
+                            data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index]}/>
+                            ))
+                        }
+                    </Pie>
+                    <Legend/>
+                    <Tooltip/>
+                </PieChart>
+            </ResponsiveContainer>
+        </React.Fragment>
+    );
+}
---
+++ client/src/components/ManagerSignUp.js
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+
+const theme = createTheme();
+
+export default function ManagerSignUp() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    const [relation, setRelation] = React.useState('가족');
+
+    const handleChange = (event) => {
+        setRelation(event.target.value);
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        보호자 회원가입
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="userID"
+                                    label="아이디"
+                                    name="userID"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="비밀번호"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="이름"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="성"
+                                    name="lastName"
+                                    autoComplete="family-name"
+                                />
+                            </Grid>
+                            <Grid item xs={12} display={'flex'} justifyContent={'flex-start'}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="relation">사용자와의 관계</InputLabel>
+                                    <Select
+                                        labelId="relation"
+                                        id="relation"
+                                        value={relation}
+                                        label="사용자와의 관계"
+                                        onChange={handleChange}
+                                        sx={{}}
+                                    >
+                                        <MenuItem value={'가족'}>가족</MenuItem>
+                                        <MenuItem value={'기관'}>기관</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="이메일"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="phone"
+                                    label="전화번호"
+                                    name="phone"
+                                    autoComplete="tel"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="productID"
+                                    label="제품 번호"
+                                    name="productID"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}
+                        >
+                            회원가입
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    이미 계정이 있으신가요? 로그인하세요
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
