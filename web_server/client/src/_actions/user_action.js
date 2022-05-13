472369c1f959b42b7540f8453fbf6e3ae01ccfb5
HideOnHouse <fffuro@kookmin.ac.kr>
2022-05-13 09:09:58 +0900

    fix api post url

--- web_server/client/src/_actions/user_action.js
+++ web_server/client/src/_actions/user_action.js
@@ -2,7 +2,7 @@ import {LOGIN_USER, REGISTER_USER} from './types';
 import axios from 'axios';
 
 export function registerUser(submitData) {
-    const request = axios.post('/api/users/register', submitData).then((response) => response.data);
+    const request = axios.post('/register', submitData).then((response) => response.data);
 
     return {
         type: REGISTER_USER,
@@ -11,7 +11,7 @@ export function registerUser(submitData) {
 }
 
 export function loginUser(submitData) {
-    const request = axios.post('/api/users/login', submitData)
+    const request = axios.post('/login', submitData)
         .then(response => response.data)
     return {
         type: LOGIN_USER,
